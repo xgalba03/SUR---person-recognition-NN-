42c9570dd0f43773f0b013795f6b18006da0e222
Šimon Pomykal <s.pomykal@quadient.com>
2022-04-30 00:17:36 +0200

    v2.0 with augmented training data

--- IKR_demos_py/img_class_nn.py
+++ IKR_demos_py/img_class_nn.py
@@ -1,67 +1,101 @@
+import timeit
+from os import path
 from statistics import mean
 
 import cv2
 import numpy as np
 import scipy
 from keras.applications.densenet import layers
+from keras.layers import Dropout
 from keras.models import Sequential
 from matplotlib import figure, pyplot as plt
 from numpy.random import randint
-from projekt_lib import png_load
+from projekt_lib import png_load, eval_load
 import tensorflow as tf
 from tensorflow.keras.optimizers import RMSprop, SGD, Adam
 import pandas as pd
 
-# Read images from target directories to 80x80 grayscale
-train_n,f = png_load('train_data/non_target_train')
-train_t,f = png_load('train_data/target_train')
-test_n,f = png_load('train_data/non_target_dev')
-test_t,f = png_load('train_data/target_dev')
 
-# label vector to determine class of img
-train_labels= np.r_[np.ones(len(train_t)), np.zeros(len(train_n))]
-test_labels= np.r_[np.ones(len(test_t)), np.zeros(len(test_n))]
-train_labels= tf.keras.utils.to_categorical(train_labels, 2)
-test_labels= tf.keras.utils.to_categorical(test_labels, 2)
+def train_model():
+    start = timeit.timeit()
+    # Read images from target directories to 80x80 grayscale
+    train_n = png_load('train_data/non_target_train', 50)
+    train_t = png_load('train_data/target_train', 30)
+    test_n = png_load('train_data/non_target_dev', 5)
+    test_t = png_load('train_data/target_dev', 5)
+
+    # label vector to determine class of img
+    train_labels = np.r_[np.ones(len(train_t)), np.zeros(len(train_n))]
+    test_labels = np.r_[np.ones(len(test_t)), np.zeros(len(test_n))]
+    train_labels = tf.keras.utils.to_categorical(train_labels, 2)
+    test_labels = tf.keras.utils.to_categorical(test_labels, 2)
+
+    # merge classes
+    train_data = np.r_[train_t, train_n]
+    test_data = np.r_[test_t, test_n]
+    """
+    best yet
+    model = Sequential()
+    model.add(layers.Conv2D(filters=6, kernel_size=(5, 5), activation='relu', input_shape=(80, 80, 3)))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Conv2D(filters=16, kernel_size=(5, 5), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(units=40, activation='relu'))
+    model.add(layers.Dense(units=2, activation='softmax'))
+    """
+
+    model = Sequential()
+    model.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu', input_shape=(80, 80, 3)))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.4))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(units=4096, activation='relu'))
+    model.add(layers.Dense(units=4096, activation='relu'))
+    model.add(layers.Dense(units=2, activation='softmax'))
+
+    batch_size = 20  # 20
+    steps = len(train_data) // batch_size
+    epochs = 11  # 11
+
+    model.summary()
+    model.compile(
+        optimizer=RMSprop(),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    history = model.fit(
+        train_data, train_labels,
+        batch_size=batch_size,
+        steps_per_epoch=steps,
+        epochs=epochs,
+        verbose=1,
+        validation_data=(test_data, test_labels)
+    )
+    end = timeit.timeit()
+    duration = end - start
+    print("Training dataset size:", len(train_data))
+    print("Model training time: ", duration)
+
+    model.save('my_model')
+    return model
 
 
-train_data = np.r_[train_t, train_n]
-test_data = np.r_[test_t, test_n]
+if __name__ == "__main__":
 
-model = Sequential()
-model.add(layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=(80, 80, 1)))
-model.add(layers.AveragePooling2D())
-model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
-model.add(layers.AveragePooling2D())
-#model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
-#model.add(layers.AveragePooling2D())
-model.add(layers.Flatten())
-#model.add(layers.Dense(units=300, activation='relu'))
-model.add(layers.Dense(units=120, activation='relu'))
-model.add(layers.Dense(units=84, activation='relu'))
-model.add(layers.Dense(units=40, activation='relu'))
-model.add(layers.Dense(units=2, activation='softmax'))
+    model = Sequential()
+    if path.isdir('my_model'):
+        model = tf.keras.models.load_model("my_model")
 
-batch_size = 30  # 30
-epochs = 10  # 15
+    else:
+        model = train_model()
 
-model.summary()
-model.compile(
-    optimizer=RMSprop(),
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
-
-history = model.fit(
-    train_data, train_labels,
-    batch_size=batch_size,
-    epochs=epochs,
-    verbose=1,
-    validation_data=(test_data, test_labels)
-)
-
-eval_data, files = png_load('eval')
-est = model.predict(eval_data)
-df = pd.DataFrame(est)
-df['filename'] = files
-print(est)
+    eval_data, files = eval_load('eval')
+    est = model.predict(eval_data)
+    df = pd.DataFrame(est)
+    df['filename'] = files
+    print(est)
--- IKR_demos_py/projekt_lib.py
+++ IKR_demos_py/projekt_lib.py
@@ -1,10 +1,13 @@
 from glob import glob
+from random import random
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.io import wavfile
 from ikrlib import mfcc
 from imageio import imread
+import albumentations as A
 
 
 def test():
@@ -38,16 +41,73 @@ def png2fea(dir_name):
     return np.array(data)
 
 
-def png_load(dir_name):
+def display_image(im_data):
+    dpi = 80
+    height, width, depth = im_data.shape
+
+    # What size does the figure need to be in inches to fit the image?
+    figsize = width / float(dpi), height / float(dpi)
+
+    # Create a figure of the right size with one axes that takes up the full figure
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_axes([0, 0, 1, 1])
+
+    # Hide spines, ticks, etc.
+    ax.axis('off')
+
+    # Display the image.
+    ax.imshow(im_data, cmap='gray')
+
+    plt.show()
+
+
+def augment_img(image):
+    transform = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+        A.ShiftScaleRotate(shift_limit=0, scale_limit=(0, 0.1), rotate_limit=45, p=0.50),
+        A.Blur(blur_limit=3)
+    ])
+    augmented_image = transform(image=image)['image']
+    return augmented_image
+
+
+def png_load(dir_name, augment_count=0):
+    data = []
+    for f in glob(dir_name + '/*.png'):
+        # print('Processing file: ', f)
+        # features.append(cv2.imread(f, cv2.IMREAD_GRAYSCALE).astype(np.float64))
+        # img = cv2.imread(f, cv2.IMREAD_COLOR).astype(np.float64)
+        # img = cv2.imread(f, cv2.IMREAD_GRAYSCALE).astype(np.float64)
+
+        image = cv2.imread(f)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img = image / 255
+        data.append(img)
+
+        # display_image(image)
+
+        for x in range(augment_count):
+            augmented_image = augment_img(image)
+            img_a = augmented_image / 255
+            data.append(img_a)
+            # display_image(augmented_image)
+
+    return np.array(data)
+
+
+def eval_load(dir_name):
     data = []
     file_names = []
     for f in glob(dir_name + '/*.png'):
-        print('Processing file: ', f)
+        # print('Processing file: ', f)
         file_names.append(f)
         # features.append(cv2.imread(f, cv2.IMREAD_GRAYSCALE).astype(np.float64))
         #img = cv2.imread(f, cv2.IMREAD_COLOR).astype(np.float64)
-        img = cv2.imread(f, cv2.IMREAD_GRAYSCALE).astype(np.float64)
-        img = img / 255
+        # img = cv2.imread(f, cv2.IMREAD_GRAYSCALE).astype(np.float64)
+        image = cv2.imread(f)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img = image / 255
         data.append(img)
 
     return np.array(data), np.array(file_names)
